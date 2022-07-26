e8160d0ecab832bff7767f6afa1f7e2dc71360b3
ducvnguyen97 <ducvnguyen97@gmail.com>
2022-07-26 21:12:33 +0700

    init file

---
+++ Introduction.py
@@ -0,0 +1,34 @@
+'''
+1. Introduction
+    1.1. What is Python
+        - Python is a programming language. It was created by Guido van Rossum and released in 1991.
+        - It is used for:
+            - web development (server-side)
+            - software development
+            - mathematics
+            - system scripting
+    1.2. What can Python do
+        - Python can be used on a server to creat web application
+        - Python can be used along side software to create workflows
+        - Python can connect to database systems. Its can also read and modify files
+        - Python can be used to hanlde big data and perform comple mathematics
+        - Python can be used for rapod prototyping, or for production-ready software development
+    1.3. Why python
+        - Python works on different platforms (window, mac, linux, raspberry pi, etc..)
+        - Python has a simple syntax similar to the English language
+        - Python has syntax that allows developers to write programs with fewer line than some other language
+        - Python runs on an interpreter system, meaning that code can be executed as soon as it is written.
+           --> this mean that prototyping can be very quick
+        - Pythone can be treated in a procedural way, an object-oriented way or functional way.
+    1.4. Good to know
+        - Python 3 is mose rencet major version.
+        - Python 2 not being updated with anything, but it is still quite popular.
+    1.5. Python syntax compared to other programming languages
+        - Python was designed for readability, and has some similarites to the english language with influence form mathematics
+        - Python uses new line to complete a com,and, as opposed to other programming languages which often use semicolons or parentheses.
+        - Python relies on indentation, using whitespace, to define scope; such as the scopes of loops, functions and classes.
+            other programming languages often use curly-brackets for this purpose.
+'''
+'''
+Reference and Source: W3Scholl
+'''
---
+++ Python_syntax.py
@@ -0,0 +1,43 @@
+'''
+1. Syntax
+    1.1 Execute python syntax
+        - Command line
+        - creating python file on the server and run it in command line
+    1.2. Python indentation
+        - Indentation refers to the spaces at the beginning of a code line
+        - Where in other programming language the indentation in code is for readability only, the indentation in python is very important.
+        --> Python will give you an error if you skip the indentation
+        - The numbers of spaces is up to you as a programer, the most common use is four, but it has to be at least one
+        - You have to use the same number of spaces in the same block of code, otherwise python give you an error
+    1.3. Variables
+        - Variables are created when you assign a value to it.
+        - Python has no command for declaring a variable
+    1.4. Comments
+        - Pythons has commmeting capability for the purpose of in code documentation
+        - Comments start with a #, and python will render the rest of the line as a comment
+        - Comments can be use to explain python code, make the code more readable, prevent execution when testing code
+        - Multiline comments:
+        - Python will ignore string literals that are not assigned to a variables, 
+        -->you can add a multiline string (triple quotes) in your code, and place your comment inside it
+2. Variables
+    2.1. Creating variables
+        - Python has no command for declaring a variable
+        - A variable is created the moments you first assign a value to it
+         e.g:
+            x = 5
+            y = "Jone"
+            print(x)
+            print(y)
+        - Variable do not need to be declared with any particular type,
+            and can even change type after they have been set.
+    2.2. Casting
+        - If you want to specify the data tyoe if a variable, this can be done with casting
+        - e.g:
+            x = str(3)    # x will be '3'
+            y = int(3)    # y will be 3
+            z = float(3)  # z will be 3.0
+    2.3 Get the type
+        -Y
+
+''' 
+# Reference and Source: W3School
